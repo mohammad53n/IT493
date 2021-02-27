a1ca2a87cefd7fd220ee85259dc6d7f54a7045d2
mohammad53n <79766541+mohammad53n@users.noreply.github.com>
2021-02-27 15:10:18 -0500

    Create employee-dashboard.php

---
+++ employee-dashboard.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Employee Dashboard</title>
+
+</head>
+
+<body>
+<?php
+
+//server address, username, password, db name
+$connection=mysqli_connect("main-routes-customers.cojpszz7ysj9.us-east-1.rds.amazonaws.com","admin","493Class2021!","main-routes-customers");
+
+if ($connection) {
+  echo "Connection established";
+} else {
+  die("Connection failed. Reason: ".mysqli_connect_error());
+}
+
+
+
+
+?>
+
+</body>
+
+</html>
