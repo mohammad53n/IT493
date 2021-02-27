5d898fc6dabc5062b8c2dadc0eecd43000eddea3
mohammad53n <79766541+mohammad53n@users.noreply.github.com>
2021-02-27 15:14:25 -0500

    Update employee-dashboard.php

--- employee-dashboard.php
+++ employee-dashboard.php
@@ -12,14 +12,11 @@
 //server address, username, password, db name
 $connection=mysqli_connect("main-routes-customers.cojpszz7ysj9.us-east-1.rds.amazonaws.com","admin","493Class2021!","main-routes-customers");
 
-if ($connection) {
-  echo "Connection established";
-} else {
-  die("Connection failed. Reason: ".mysqli_connect_error());
-}
-
-
-
+  if ($connection) {
+    echo "Connection established";
+  } else {
+    die("Connection failed. Reason: ".mysqli_connect_error());
+  }
 
 ?>
 
