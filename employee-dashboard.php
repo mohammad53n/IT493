2aa95030252f66b98b296151d70c38ba58079e81
mohammad53n <79766541+mohammad53n@users.noreply.github.com>
2021-03-06 12:08:16 -0500

    Update employee-dashboard.php

--- employee-dashboard.php
+++ employee-dashboard.php
@@ -7,19 +7,20 @@
 </head>
 
 <body>
+  
 <?php
-//server address, username, password, db name
-$connection=mysqli_connect("main-routes-customers.cojpszz7ysj9.us-east-1.rds.amazonaws.com","admin","493Class2021!","main-routes-customers");
+//server address, username, password
+$connection = new mysqli("main-routes-customers.cojpszz7ysj9.us-east-1.rds.amazonaws.com","admin","493Class2021!");
 
   if ($connection) {
-    echo "Connection established";
+    echo "Connection to AWS DB server established";
   } else {
     echo("Connection failed. Reason: ".mysqli_connect_error());
   }
 echo "<br>";
 
 $ip_server = $_SERVER['SERVER_ADDR']; 
-echo "Server IP Address is: $ip_server"; 
+echo "Shahi Kulfi server IP Address is: $ip_server"; 
 
 ?>
 
